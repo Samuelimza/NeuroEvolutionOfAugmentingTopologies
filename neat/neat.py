e09187434e730e586b32cb39d20fa6d598277f99
Samuelimza <osamaazmi1999@gmail.com>
2019-05-20 20:53:14 +0530

    setting up

---
+++ neat/neat.py
@@ -0,0 +1,21 @@
+class neatclass():
+	def __init__(self):
+		pass
+
+	def speciate():
+		pass
+
+	def select():
+		pass
+
+	def crossover():
+		pass
+
+	def mutate():
+		pass
+
+	def train():
+		pass
+
+	def giveNNs():
+		pass
